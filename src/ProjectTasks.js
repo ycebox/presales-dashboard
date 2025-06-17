e0d642f4b863c5a5934c27f626f0cf4bd0be5a1e
ycebox <jbautista.bpc@gmail.com>
2025-06-17 15:14:24 +0800

    Update ProjectTasks.js

--- src/ProjectTasks.js
+++ src/ProjectTasks.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import supabase from "./supabaseClient";
+import { supabase } from './supabaseClient';
 
 export default function ProjectTasks({ projectId }) {
   const [tasks, setTasks] = useState([]);
