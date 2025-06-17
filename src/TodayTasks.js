98edacd29b35272637252c840c4b780eee6697a8
ycebox <jbautista.bpc@gmail.com>
2025-06-17 15:14:56 +0800

    Update TodayTasks.js

--- src/TodayTasks.js
+++ src/TodayTasks.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import supabase from "./supabaseClient";
+import { supabase } from './supabaseClient';
 
 export default function TodayTasks() {
   const [tasks, setTasks] = useState([]);
