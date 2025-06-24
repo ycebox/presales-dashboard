95cb198effa11ca879307332453f977fbdcd8da4
ycebox <jbautista.bpc@gmail.com>
2025-06-24 14:32:56 +0800

    Update ProjectDetails.js

--- src/ProjectDetails.js
+++ src/ProjectDetails.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import supabase from './supabaseClient';
+import { supabase  } from './supabaseClient';
 
 function ProjectDetails() {
   const { id } = useParams();
