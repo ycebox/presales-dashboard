1286a4c875d318451042b2138a5a7d4c9fd6fb85
ycebox <jbautista.bpc@gmail.com>
2025-06-29 16:19:14 +0800

    Update Projects.js

--- src/Projects.js
+++ src/Projects.js
@@ -116,45 +116,47 @@ function Projects() {
       {loading ? (
         <p>Loading...</p>
       ) : (
-        <div className="table-container">
-          <table className="modern-table">
-            <thead>
-              <tr>
-                <th>Customer</th>
-                <th>Country</th>
-                <th>Account Manager</th>
-                <th>Sales Stage</th>
-                <th>Product</th>
-                <th>Scope</th>
-                <th>Backup</th>
-                <th>Remarks</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {projects.map((project) => (
-                <tr key={project.id}>
-                  <td>
-                    <Link to={`/project/${project.id}`} className="project-link">
-                      {project.customer_name}
-                    </Link>
-                  </td>
-                  <td>{project.country}</td>
-                  <td>{project.account_manager}</td>
-                  <td>{project.sales_stage}</td>
-                  <td>{project.product}</td>
-                  <td>{project.scope}</td>
-                  <td>{project.backup_presales}</td>
-                  <td>{project.remarks}</td>
-                  <td>
-                    <button className="delete-btn" onClick={() => handleDeleteProject(project.id)}>
-                      <FaTrash />
-                    </button>
-                  </td>
+        <div className="table-scroll-wrapper">
+          <div className="table-container">
+            <table className="modern-table">
+              <thead>
+                <tr>
+                  <th>Customer</th>
+                  <th>Country</th>
+                  <th>Account Manager</th>
+                  <th>Sales Stage</th>
+                  <th>Product</th>
+                  <th>Scope</th>
+                  <th>Backup</th>
+                  <th>Remarks</th>
+                  <th>Actions</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {projects.map((project) => (
+                  <tr key={project.id}>
+                    <td>
+                      <Link to={`/project/${project.id}`} className="project-link">
+                        {project.customer_name}
+                      </Link>
+                    </td>
+                    <td>{project.country}</td>
+                    <td>{project.account_manager}</td>
+                    <td>{project.sales_stage}</td>
+                    <td>{project.product}</td>
+                    <td>{project.scope}</td>
+                    <td>{project.backup_presales}</td>
+                    <td>{project.remarks}</td>
+                    <td>
+                      <button className="delete-btn" onClick={() => handleDeleteProject(project.id)}>
+                        <FaTrash />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       )}
 
