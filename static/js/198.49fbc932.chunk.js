19c29b55fb4654b481e2b861c35c850cba6e71f7
ycebox <ycebox@users.noreply.github.com>
2025-07-01 06:25:04 +0000

    deploy: 05e24da6fb26627c8d19d04245054e25673c23eb

---
+++ static/js/198.49fbc932.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpresales_dashboard=self.webpackChunkpresales_dashboard||[]).push([[198],{198:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=198.49fbc932.chunk.js.map
