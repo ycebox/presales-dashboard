471e634319a496c389d52ff64a7435dd4aea6d67
jonathan <jbautista.bpc@gmail.com>
2026-01-02 12:41:58 +0800

    Update ReportsDashboard.js

--- src/ReportsDashboard.js
+++ src/ReportsDashboard.js
@@ -1,5 +1,5 @@
 // ReportsDashboard.js
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react'; 
 import {
   FaChartLine,
   FaTrophy,
